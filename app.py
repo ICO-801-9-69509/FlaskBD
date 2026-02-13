d583a074925b50dbc683e44c0ba4981e9fdd29ed
David Chessington <davidadavaz@outlook.com>
2026-02-13 09:57:41 -0600

    forms 13/02/2026

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+from forms import UserForm
 
 app=flask.Flask("__main__")
 
@@ -6,5 +7,10 @@ app=flask.Flask("__main__")
 def main():
     return flask.render_template("index.html")
 
+@app.route("/usuarios")
+def usuarios():
+    form=UserForm()
+    return flask.render_template("usuarios.html",form=form)
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
---
+++ forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form, StringField,IntegerField,EmailField,PasswordField,SubmitField, validators
+
+
+
+class UserForm(Form):
+    nombre=StringField("Nombre")
+    apaterno=StringField("Apellido Paterno")
+    amaterno=StringField("Apellido Materno")
+    edad=IntegerField("Edad")
+    correo=EmailField("Email")
+
