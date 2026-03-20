46913543be4a87cbefa9824e0234904c6e98c71e
David Chessington <davidadavaz@outlook.com>
2026-03-19 22:04:08 -0600

    refactor: update models and routes alumnos and maestros

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import flask
 from forms import TeacherForm, UserForm
 from flask_wtf.csrf import CSRFProtect
-from models import db, Alumnos, Maestros
+from models import db, Alumno, Maestro, Inscripcion, Curso
 from config import DevelopmentConfig
 from flask_migrate import Migrate
 
@@ -14,20 +14,23 @@ migrate=Migrate(app,db)
 #flask db upgrade realiza los cambios
 csrf=CSRFProtect(app)
 
-@app.route("/",methods=["GET","POST"])
 @app.route("/index")
+@app.route("/")
 def index():
+    return flask.render_template("index.html")
+
+@app.route("/alumnos",methods=["GET","POST"])
+def alumnos():
     create_alumno =UserForm(flask.request.form)
     #select * from alumnos
-    alumno=Alumnos.query.all()
-    return flask.render_template("index.html",form=create_alumno,alumno=alumno)
+    alumno=Alumno.query.all()
+    return flask.render_template("alumnos.html",form=create_alumno,alumno=alumno)
 
 @app.route("/maestros",methods=["GET","POST"])
 def maestros():
     create_maestro=TeacherForm(flask.request.form)
-    maestro=Maestros.query.all()
+    maestro=Maestro.query.all()
     return flask.render_template("maestros.html",form=create_maestro,maestro=maestro)
-
 
 @app.route("/usuarios",methods=["GET","POST"])
 def usuario():
